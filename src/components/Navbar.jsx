c58f7f196bc8c2a8771eaa774871b76e773f091e
francaparroz21 <francaparroz21@gmail.com>
2023-01-22 22:55:09 -0300

    solucionamos collapse bootstrap,navbar terminada

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,41 +9,34 @@ const ListNavbar = (props) => {
 
 const LogoItem = (props) => {
     return (
-        <a className="nav-link" aria-controls={props.ariacontrols} data-bs-target={props.target} data-bs-toggle={props.toggle} type={props.type} id={props.id} href={props.route}><img src={props.routeImg} alt={props.alt} /></a>
+        <a aria-controls={props.ariacontrols} data-bs-target={props.target} data-bs-toggle={props.toggle} type={props.type} id={props.id} href={props.route}><img src={props.routeImg} alt={props.alt} /></a>
     )
 }
 
 const Navbar = () => {
     return (
-        <nav className="fixed-top bg-body-tertiary navbar navbar-expand-md">
+        <nav className="navbar navbar-expand-md">
             <div className="navbar-left">
                 <div>
                     <LogoItem route="./index.html" routeImg={logo} />
                 </div>
-                <div id="collapse_items" className="collapse navbar-collapse">
-                    <ul className="navbar-nav ms-3">
-                        <div>
-                            <ListNavbar className="nav-item" content="Inicio" route="./index.html" />
-                        </div>
-                        <div>
-                            <ListNavbar className="nav-item" content="Catalogo" route="./pages/catalogue.html" />
-                        </div>
-                        <div>
-                            <ListNavbar className="nav-item" content="Servicios" route="./pages/services.html" />
-                        </div>
+                <div className="collapse navbar-collapse" id="options">
+                    <ul className="navbar-nav">
+                        <ListNavbar className="nav-item" content="Inicio" route="./index.html" />
+                        <ListNavbar className="nav-item" content="Catalogo" route="./" />
+                        <ListNavbar className="nav-item" content="Servicios" route="./" />
                     </ul>
                 </div>
             </div>
+            <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#options">
+                <span className="navbar-toggler-icon"></span>
+            </button>
             <div className="navbar-right">
                 <div>
                     <LogoItem id='cartIconToggle' type="button" toggle="offcanvas" target="#offcanvasWithBackdrop"
                         ariacontrols="offcanvasWithBackdrop" routeImg={cart} />
                 </div>
             </div>
-            <button type="button" className="navbar-toggler" data-bs-toggle="collapse modal" data-bs-target="#collapse_items">
-                <span className="navbar-toggler-icon">
-                </span>
-            </button>
         </nav>
     );
 }
