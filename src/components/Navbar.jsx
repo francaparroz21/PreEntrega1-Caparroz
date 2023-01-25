433a0934df6e97dec149841cc71a260ae019d2c6
francaparroz21 <francaparroz21@gmail.com>
2023-01-25 03:00:55 -0300

    corrigiendo estructura de index.html y componente Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,29 +5,31 @@ import cart from "../assets/cart.png"
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-md">
-            <div className="navbar-left">
-                <div>
-                    <ItemListContainer route="./index.html" routeImg={logo} />
+        <header>
+            <nav className="navbar navbar-expand-md">
+                <div className="navbar-left">
+                    <div>
+                        <ItemListContainer route="./index.html" routeImg={logo} />
+                    </div>
+                    <div className="collapse navbar-collapse" id="options">
+                        <ul className="navbar-nav">
+                            <ItemListContainer className="nav-item" content="Inicio" route="./index.html" />
+                            <ItemListContainer className="nav-item" content="Catalogo" route="./" />
+                            <ItemListContainer className="nav-item" content="Servicios" route="./" />
+                        </ul>
+                    </div>
                 </div>
-                <div className="collapse navbar-collapse" id="options">
-                    <ul className="navbar-nav">
-                        <ItemListContainer className="nav-item" content="Inicio" route="./index.html" />
-                        <ItemListContainer className="nav-item" content="Catalogo" route="./" />
-                        <ItemListContainer className="nav-item" content="Servicios" route="./" />
-                    </ul>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#options">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="navbar-right">
+                    <div>
+                        <CartWidget id='cartIconToggle' type="button" toggle="offcanvas" target="#offcanvasWithBackdrop"
+                            ariacontrols="offcanvasWithBackdrop" routeImg={cart} />
+                    </div>
                 </div>
-            </div>
-            <button  className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#options">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="navbar-right">
-                <div>
-                    <CartWidget id='cartIconToggle' type="button" toggle="offcanvas" target="#offcanvasWithBackdrop"
-                        ariacontrols="offcanvasWithBackdrop" routeImg={cart} />
-                </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     );
 }
 export default Navbar
